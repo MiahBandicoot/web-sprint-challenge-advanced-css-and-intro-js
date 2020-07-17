e3e77b829a822ba27a5e90331d8db4cd8d42f5c3
Miah <revolution5117@gmail.com>
2020-07-17 08:49:05 -0700

    questions 4 and 7 remianing

--- index.js
+++ index.js
@@ -207,13 +207,16 @@ const artists = [
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
+console.log(artists[0])
+console.log(artists[2])
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
-
-
+function changeName(array,index,name){
+array[index].name=name;
+return array;
+}
+console.log(changeName(artists,8,'Vincent Van Gogh'))
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
  *     (2) a number which is the desired index in the array.
@@ -223,9 +226,9 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+  return `The artist at index ${array[index].id} is ${array[index].name}.`
   }
-  
+  console.log(getArtistByIndex(artists,0))
   /**
 
 
@@ -248,11 +251,16 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(array,index) {
+    for(let i=0; i< array.length; i++){
+      if(array[i]===(index)){
+      array.splice(i,1)
+    }
   }
+  return array
+}
   
- 
+ console.log(removeArtist(artists[0]))
 
 /**
 /* Task 6: Create a function called `addArtist` that can accept an object of information and add it to the artists array. Then, add a 21st artist to the array (you) with custom information!👩‍🎨👨‍🎨
@@ -267,12 +275,12 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/* Code here */){
-
-    /* Code here */
-
+function addArtist(array,id,name,years,genre,nationality,bio,wikipedia,paintings){
+  array.push({id,name,years,genre,nationality,bio,wikipedia,paintings});
+   return array;
+  
   }
-
+console.log(addArtist(artists,"20","Miah Davis","1994-9999+","Symolism","American"," Inspired by the country's popular culture, he deliberately stood aloof from contemporary art trends. His work contains fantastic illustrations of religious concepts and narratives. Within his lifetime his work was collected in the Frida Kahlo Museum,Mexico City, Chapingo, Cuernavaca, San Francisco, Detroit, and New York City."))
 /* Task 7: Create a function called lotsOfArt() that takes one argument: 
 
     (1) artists array 
